8d353da503145d5372d5ce87bab7b74029945812
strive <2205487298@qq.com>
2025-02-21 12:06:17 +0800

    update

---
+++ src/main/java/com/strive/ChapterI/Sample3.java
@@ -0,0 +1,34 @@
+package com.strive.ChapterI;
+
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class Sample3 {
+
+    static Logger logger = init(Level.DEBUG);
+
+    public static void main(String[] args) {
+        logger.debug("{}",expensive());
+    }
+
+    static String expensive() {
+        System.out.println("执行耗时操作...");
+        return "日志";
+    }
+
+    static <AppenderComponentBuilder, BuiltConfiguration> Logger init(Level level) {
+        ConfigurationBuilder <BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder()
+                .setStatusLevel(Level.ERROR)
+                .setConfigurationName("BuilderTest");
+        AppenderComponentBuilder appender =
+                builder.newAppender("Stdout","CONSOLE")
+                        .addAttribute("target", ConsoleAppender.Target.SYSTEM_OUT)
+                        .add(builder.newLayout("PatternLayout").addAttribute("pattern","%d [%t] %-5level:%smsg%n%throwab"));
+        bulider.add(appender)
+                .add(bulider.newRootLogger(level).add(bulider.newAppenderRef("Stout")));
+        Configurator.initialize(bulider.build());
+        return LogManager.getLogger();
+    }
+
+}
