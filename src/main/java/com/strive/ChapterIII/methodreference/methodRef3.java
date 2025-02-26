33dd1e4bc373e554edbe9faa2a8e78f9d4f58f18
strive <2205487298@qq.com>
2025-02-26 16:34:54 +0800

    update

---
+++ src/main/java/com/strive/ChapterIII/methodreference/methodRef3.java
@@ -0,0 +1,14 @@
+package com.strive.ChapterIII.methodreference;
+
+public class methodRef3 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    record Student(String name,String sex) {
+
+    }
+
+}
