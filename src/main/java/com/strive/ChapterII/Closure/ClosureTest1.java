d3564299b643d2f28ba0f295071546ea45c5609e
strive <2205487298@qq.com>
2025-02-27 14:09:57 +0800

    update

---
+++ src/main/java/com/strive/ChapterII/Closure/ClosureTest1.java
@@ -0,0 +1,48 @@
+package com.strive.ChapterII.Closure;
+
+//闭包
+//可以使用静态变量 成员变量 方法参数变量  不能使用非静态变量
+public class ClosureTest1 {
+
+    @FunctionalInterface
+    interface Lambda {
+        int op(int y);
+    }
+
+    static void Order(Lambda lambda) {
+        System.out.println(lambda.op(1));
+    }
+
+    static int a = 10;
+    int b = 3;
+
+    public static void main(String[] args) {
+        final int x = 10;
+        Order((int y) -> x + y);
+        Order((int y) -> a + y);
+        //闭包不能使用非静态变量
+        //Order((int y) -> b + y);
+        Student stu = new Student(18);
+        Lambda lambda = y -> y + stu.d;
+        Order(lambda);
+
+        stu.d = 30;
+        Order(lambda);
+    }
+
+    static class Student {
+        int d;
+
+        public Student(int d) {
+            this.d = d;
+        }
+
+    }
+
+    public void test(int c) {
+        Order(y-> a + y); //静态变量
+        Order(y-> b + y); //成员变量
+        Order(y-> c + y); //方法参数变量
+    }
+
+}
---
+++ src/main/java/com/strive/ChapterII/Closure/ClosureTest2.java
@@ -0,0 +1,31 @@
+package com.strive.ChapterII.Closure;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ClosureTest2 {
+    public static void main(String[] args) throws IOException {
+        //创建10个任务对象，并且每一个任务对象给一个任务编号
+        //任务对象需放到线程池中
+        List<Runnable> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            //i本身是变化的，不能作为闭包里的外部变量（因为要满足final或着effectively final）
+            //解决方法就是将i赋给一个新的变量保持不变 就可以组成闭包
+            int k = i + 1;
+            Runnable task = () -> System.out.println(Thread.currentThread() + "执行任务" + k);
+            list.add(task);
+        }
+
+        //创建线程池
+        ExecutorService service = Executors.newVirtualThreadPerTaskExecutor();
+        for (Runnable task : list) {
+            //向线程池中提交任务对象
+            service.submit(task);
+        }
+        //让主线程不要停止
+        System.in.read();
+    }
+}
